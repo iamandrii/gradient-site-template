5f04d747dd37c0a2ec7c994f87035e13e1b6a315
Andrii <andry.if.com@gmail.com>
2020-10-26 00:32:30 +0200

    changed background, design. added particles.js

--- script/main.js
+++ script/main.js
@@ -1,3 +1,27 @@
 function execute(link){
   window.open(link)
 }
+
+function leadingZeros(s) {
+  s = String(s);
+  if (s.length < 2) return "0" + s;
+  else return s;
+}
+
+function updateTime(){
+  let now = new Date(Date.now());
+  document.getElementById("time").innerText=leadingZeros(now.getHours())+":"+leadingZeros(now.getMinutes());
+}
+
+function ready(){
+  updateTime();
+  setInterval(updateTime, 200);
+  particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+}
+
+
+document.onreadystatechange = function(){
+  if(document.readyState==="complete") ready();
+}
